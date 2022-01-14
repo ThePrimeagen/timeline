66ef2689c364b388ae07377a0b595ba62aff1fee
mpaulson <mpaulson@netflix.com>
2022-01-14 15:25:48 -0700

    feat: query now works with context on count, selftime, and totaltime

--- src/query.rs
+++ src/query.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashSet, fs::File};
 
-use log::debug;
+use log::{debug, info};
 
 use serde::Deserialize;
 
@@ -21,12 +21,31 @@ pub struct Distance {
 }
 
 #[derive(Debug, Deserialize)]
+pub struct TotalTime {
+    node: String,
+    context: Option<Vec<String>>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct SelfTime {
+    node: String,
+    context: Option<Vec<String>>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Count {
+    root: String,
+    child_name: String,
+    context: Option<Vec<String>>,
+}
+
+#[derive(Debug, Deserialize)]
 #[serde(tag = "type")]
 pub enum Query {
     Distance(Distance),
-    SelfTime { node: String },
-    TotalTime { node: String },
-    Count { root: String, child_name: String },
+    SelfTime(SelfTime),
+    TotalTime(TotalTime),
+    Count(Count),
 }
 
 #[derive(Debug, Deserialize)]
@@ -55,6 +74,47 @@ pub fn parse_query(opts: &TelemetryTimelineOpts) -> Result<QueryConfig, Timeline
     return Ok(serde_json::de::from_reader(query_file)?);
 }
 
+fn get_contexts<'a>(
+    contexts: &'a Vec<Zone>,
+    filter: &Option<Vec<String>>,
+) -> Option<Vec<&'a Zone>> {
+    let mut found_contexts: Option<Vec<&'a Zone>> = None;
+
+    if let Some(c) = &filter {
+        found_contexts = Some(
+            contexts
+                .iter()
+                .filter(|context| {
+                    return c.contains(&context.name);
+                })
+                .collect::<Vec<&Zone>>(),
+        );
+    }
+
+    return found_contexts;
+}
+
+fn filter_nodes(node: &Node, contexts: &Option<Vec<&Zone>>) -> bool {
+    if let Some(context) = &contexts {
+        let mut ret = false;
+        for zone in context {
+            debug!(
+                "context zone({:?})#contains({:?}) = {}",
+                zone,
+                node.zone,
+                zone.contains(&node.zone)
+            );
+
+            if zone.contains(&node.zone) {
+                ret = true;
+                break;
+            }
+        }
+        return ret;
+    }
+    return true;
+}
+
 fn calculate_distance(
     froms: &[&Node],
     distance: &Distance,
@@ -64,19 +124,13 @@ fn calculate_distance(
 ) -> Vec<QueryResult> {
     return froms
         .iter()
+        // TODO: Determine if this can ever actually be an option?
         .filter(|node| {
-            if let Some(context) = &contexts {
-                let mut ret = false;
-                for zone in context {
-                    if zone.contains(&node.zone) {
-                        ret = true;
-                        break;
-                    }
-                }
-                return ret;
-            }
-
-            return true;
+            filter_nodes(
+                node.child_by_name(&distance.to)
+                    .expect("this child should always exist"),
+                contexts,
+            )
         })
         .flat_map(|c| c.time_to(&distance.to, ignores, &side))
         .map(|d| {
@@ -95,18 +149,8 @@ fn distance_query(
     query_config: &QueryConfig,
     distance: &Distance,
 ) -> Vec<QueryResult> {
-    let mut found_contexts: Option<Vec<&Zone>> = None;
-
-    if let Some(c) = &distance.context {
-        found_contexts = Some(
-            contexts
-                .iter()
-                .filter(|context| {
-                    return c.contains(&context.name);
-                })
-                .collect::<Vec<&Zone>>(),
-        );
-    }
+    info!("distance_query");
+    let found_contexts = get_contexts(contexts, &distance.context);
 
     debug!("found context: {:?}", found_contexts);
 
@@ -148,6 +192,74 @@ fn distance_query(
         .collect::<Vec<QueryResult>>();
 }
 
+fn self_time_query(
+    roots: &Vec<Node>,
+    contexts: &Vec<Zone>,
+    self_time: &SelfTime,
+) -> Vec<QueryResult> {
+    let found_contexts = get_contexts(contexts, &self_time.context);
+
+    return roots
+        .iter()
+        .flat_map(|r| r.nodes_by_name(&self_time.node))
+        .filter(|n| filter_nodes(n, &found_contexts))
+        .map(|n| {
+            return QueryResult {
+                measurement_type: "SelfTime".to_string(),
+                name: self_time.node.clone(),
+                count: n.calc_self_time(),
+            };
+        })
+        .collect();
+}
+
+fn total_time_query(
+    roots: &Vec<Node>,
+    contexts: &Vec<Zone>,
+    total_time: &TotalTime,
+    query_config: &QueryConfig,
+) -> Vec<QueryResult> {
+    let found_contexts = get_contexts(contexts, &total_time.context);
+    return roots
+        .iter()
+        .flat_map(|r| r.nodes_by_name(&total_time.node))
+        .filter(|n| filter_nodes(n, &found_contexts))
+        .map(|n| {
+            return QueryResult {
+                measurement_type: "TotalTime".to_string(),
+                name: total_time.node.clone(),
+                count: n.calc_total_time(&query_config.ignores),
+            };
+        })
+        .collect();
+}
+
+fn count_query(
+    roots: &Vec<Node>,
+    contexts: &Vec<Zone>,
+    count: &Count,
+) -> Vec<QueryResult> {
+    let found_contexts = get_contexts(contexts, &count.context);
+    return roots
+        .iter()
+        .flat_map(|r| r.nodes_by_name(&count.root))
+        .map(|n| {
+            n.nodes_by_name(&count.child_name)
+                .iter()
+                .filter(|n| filter_nodes(n, &found_contexts))
+                .count()
+        })
+        .filter(|&c| c > 0)
+        .map(|c| {
+            return QueryResult {
+                measurement_type: "Count".to_string(),
+                name: format!("{}-{}", count.root, count.child_name),
+                count: c as u64,
+            };
+        })
+        .collect();
+}
+
 pub fn run_query(
     roots: &Vec<Node>,
     query: &Query,
@@ -159,49 +271,9 @@ pub fn run_query(
 
     match query {
         Query::Distance(d) => distance_query(roots, contexts, query_config, d),
-        Query::SelfTime { node } => {
-            roots
-                .iter()
-                .flat_map(|r| r.nodes_by_name(node))
-                .map(|n| {
-                    return QueryResult {
-                        measurement_type: "SelfTime".to_string(),
-                        name: node.clone(),
-                        count: n.calc_self_time(),
-                    };
-                })
-                .collect()
-        }
-
-        Query::TotalTime { node } => {
-            roots
-                .iter()
-                .flat_map(|r| r.nodes_by_name(node))
-                .map(|n| {
-                    return QueryResult {
-                        measurement_type: "TotalTime".to_string(),
-                        name: node.clone(),
-                        count: n.calc_total_time(&query_config.ignores),
-                    };
-                })
-                .collect()
-        }
-
-        Query::Count { root, child_name } => {
-            roots
-                .iter()
-                .flat_map(|r| r.nodes_by_name(root))
-                .map(|n| n.nodes_by_name(&child_name).len())
-                .filter(|&c| c > 0)
-                .map(|c| {
-                    return QueryResult {
-                        measurement_type: "Count".to_string(),
-                        name: format!("{}-{}", root, child_name),
-                        count: c as u64,
-                    };
-                })
-                .collect()
-        }
+        Query::SelfTime(s) => self_time_query(roots, contexts, s),
+        Query::TotalTime(t) => total_time_query(roots, contexts, t, query_config),
+        Query::Count(c) => count_query(roots, contexts, c),
     }
 }
 
@@ -210,9 +282,8 @@ mod test {
     use super::*;
     use crate::{node::build_trees, zone::Zone};
 
-    #[test]
-    fn test_run_query() {
-        let zones = vec![
+    fn get_zones() -> Vec<Zone> {
+        return vec![
             Zone::new("A".to_string(), 50, 100),
             Zone::new("D".to_string(), 48, 50),
             Zone::new("B".to_string(), 68, 71),
@@ -225,7 +296,12 @@ mod test {
             Zone::new("B".to_string(), 270, 280),
             Zone::new("C".to_string(), 230, 240),
         ];
+    }
 
+    #[test]
+    fn test_distance() {
+        let _ = env_logger::builder().is_test(true).try_init();
+        let zones = get_zones();
         let query_config: QueryConfig = QueryConfig {
             root: "A".to_string(),
             zones: HashSet::new(),
@@ -274,20 +350,10 @@ mod test {
         );
     }
 
-    fn test_run_query_with_context() {
-        let zones = vec![
-            Zone::new("A".to_string(), 50, 100),
-            Zone::new("D".to_string(), 48, 50),
-            Zone::new("B".to_string(), 68, 71),
-            Zone::new("C".to_string(), 68, 70),
-            Zone::new("A".to_string(), 101, 120),
-            Zone::new("B".to_string(), 110, 112),
-            Zone::new("D".to_string(), 122, 125),
-            // this one has B (from the node diagram)
-            Zone::new("A".to_string(), 200, 300),
-            Zone::new("B".to_string(), 270, 280),
-            Zone::new("C".to_string(), 230, 240),
-        ];
+    #[test]
+    fn test_distance_with_context() {
+        let _ = env_logger::builder().is_test(true).try_init();
+        let zones = get_zones();
 
         let query_config: QueryConfig = QueryConfig {
             root: "A".to_string(),
@@ -302,9 +368,43 @@ mod test {
             &Query::Distance(Distance {
                 from: "A".to_string(),
                 to: "B".to_string(),
-                context: None,
+                context: Some(vec!["Foo".to_string()]),
                 start: true,
                 end: false,
+            }),
+            &query_config,
+            &vec![Zone::new("Foo".to_string(), 100, 115)],
+        );
+
+        assert_eq!(results.len(), 1);
+        assert_eq!(
+            results.get(0).unwrap(),
+            &QueryResult {
+                measurement_type: "Distance".to_string(),
+                name: "A-B".to_string(),
+                count: 110 - 101,
+            }
+        );
+    }
+
+    #[test]
+    fn test_self_time() {
+        let _ = env_logger::builder().is_test(true).try_init();
+        let zones = get_zones();
+
+        let query_config: QueryConfig = QueryConfig {
+            root: "A".to_string(),
+            zones: HashSet::new(),
+            queries: vec![],
+            ignores: vec![],
+        };
+
+        let roots = build_trees(zones, &query_config);
+        let results = run_query(
+            &roots,
+            &Query::SelfTime(SelfTime {
+                node: "B".to_string(),
+                context: None,
             }),
             &query_config,
             &vec![],
@@ -314,25 +414,229 @@ mod test {
         assert_eq!(
             results.get(0).unwrap(),
             &QueryResult {
-                measurement_type: "Distance".to_string(),
-                name: "A-B".to_string(),
-                count: 68 - 50,
+                measurement_type: "SelfTime".to_string(),
+                name: "B".to_string(),
+                count: 1,
             }
         );
         assert_eq!(
             results.get(1).unwrap(),
             &QueryResult {
-                measurement_type: "Distance".to_string(),
-                name: "A-B".to_string(),
-                count: 110 - 101,
+                measurement_type: "SelfTime".to_string(),
+                name: "B".to_string(),
+                count: 2,
             }
         );
         assert_eq!(
             results.get(2).unwrap(),
             &QueryResult {
-                measurement_type: "Distance".to_string(),
+                measurement_type: "SelfTime".to_string(),
+                name: "B".to_string(),
+                count: 10,
+            }
+        );
+    }
+
+    #[test]
+    fn test_self_time_with_context() {
+        let _ = env_logger::builder().is_test(true).try_init();
+        let zones = get_zones();
+
+        let query_config: QueryConfig = QueryConfig {
+            root: "A".to_string(),
+            zones: HashSet::new(),
+            queries: vec![],
+            ignores: vec![],
+        };
+
+        let roots = build_trees(zones, &query_config);
+        let results = run_query(
+            &roots,
+            &Query::SelfTime(SelfTime {
+                node: "B".to_string(),
+                context: Some(vec!["Foo".to_string()]),
+            }),
+            &query_config,
+            &vec![Zone::new("Foo".to_string(), 100, 115)],
+        );
+
+        assert_eq!(results.len(), 1);
+        assert_eq!(
+            results.get(0).unwrap(),
+            &QueryResult {
+                measurement_type: "SelfTime".to_string(),
+                name: "B".to_string(),
+                count: 2,
+            }
+        );
+    }
+
+    #[test]
+    fn test_total_time() {
+        let _ = env_logger::builder().is_test(true).try_init();
+        let zones = get_zones();
+
+        let query_config: QueryConfig = QueryConfig {
+            root: "A".to_string(),
+            zones: HashSet::new(),
+            queries: vec![],
+            ignores: vec![],
+        };
+
+        let roots = build_trees(zones, &query_config);
+        let results = run_query(
+            &roots,
+            &Query::TotalTime(TotalTime {
+                node: "A".to_string(),
+                context: None,
+            }),
+            &query_config,
+            &vec![],
+        );
+
+        assert_eq!(results.len(), 3);
+        assert_eq!(
+            results.get(0).unwrap(),
+            &QueryResult {
+                measurement_type: "TotalTime".to_string(),
+                name: "A".to_string(),
+                count: 50,
+            }
+        );
+        assert_eq!(
+            results.get(1).unwrap(),
+            &QueryResult {
+                measurement_type: "TotalTime".to_string(),
+                name: "A".to_string(),
+                count: 19,
+            }
+        );
+        assert_eq!(
+            results.get(2).unwrap(),
+            &QueryResult {
+                measurement_type: "TotalTime".to_string(),
+                name: "A".to_string(),
+                count: 100,
+            }
+        );
+    }
+
+    #[test]
+    fn test_total_time_with_context() {
+        let _ = env_logger::builder().is_test(true).try_init();
+        let zones = get_zones();
+
+        let query_config: QueryConfig = QueryConfig {
+            root: "A".to_string(),
+            zones: HashSet::new(),
+            queries: vec![],
+            ignores: vec![],
+        };
+
+        let roots = build_trees(zones, &query_config);
+        let results = run_query(
+            &roots,
+            &Query::TotalTime(TotalTime {
+                node: "A".to_string(),
+                context: Some(vec!["Foo".to_string()]),
+            }),
+            &query_config,
+            &vec![Zone::new("Foo".to_string(), 100, 120)],
+        );
+
+        assert_eq!(results.len(), 1);
+        assert_eq!(
+            results.get(0).unwrap(),
+            &QueryResult {
+                measurement_type: "TotalTime".to_string(),
+                name: "A".to_string(),
+                count: 19,
+            }
+        );
+    }
+
+    #[test]
+    fn test_count() {
+        let _ = env_logger::builder().is_test(true).try_init();
+        let zones = get_zones();
+
+        let query_config: QueryConfig = QueryConfig {
+            root: "A".to_string(),
+            zones: HashSet::new(),
+            queries: vec![],
+            ignores: vec![],
+        };
+
+        let roots = build_trees(zones, &query_config);
+        let results = run_query(
+            &roots,
+            &Query::Count(Count {
+                root: "A".to_string(),
+                child_name: "B".to_string(),
+                context: None
+            }),
+            &query_config,
+            &vec![],
+        );
+
+        assert_eq!(results.len(), 3);
+        assert_eq!(
+            results.get(0).unwrap(),
+            &QueryResult {
+                measurement_type: "Count".to_string(),
                 name: "A-B".to_string(),
-                count: 270 - 200 - (240 - 230),
+                count: 1,
+            }
+        );
+        assert_eq!(
+            results.get(1).unwrap(),
+            &QueryResult {
+                measurement_type: "Count".to_string(),
+                name: "A-B".to_string(),
+                count: 1,
+            }
+        );
+        assert_eq!(
+            results.get(2).unwrap(),
+            &QueryResult {
+                measurement_type: "Count".to_string(),
+                name: "A-B".to_string(),
+                count: 1,
+            }
+        );
+    }
+
+    #[test]
+    fn test_count_with_context() {
+        let _ = env_logger::builder().is_test(true).try_init();
+        let zones = get_zones();
+
+        let query_config: QueryConfig = QueryConfig {
+            root: "A".to_string(),
+            zones: HashSet::new(),
+            queries: vec![],
+            ignores: vec![],
+        };
+
+        let roots = build_trees(zones, &query_config);
+        let results = run_query(
+            &roots,
+            &Query::Count(Count {
+                root: "A".to_string(),
+                child_name: "B".to_string(),
+                context: Some(vec!["Foo".to_string()]),
+            }),
+            &query_config,
+            &vec![Zone::new("Foo".to_string(), 260, 280)],
+        );
+
+        assert_eq!(results.len(), 1);
+        assert_eq!(
+            results.get(0).unwrap(),
+            &QueryResult {
+                measurement_type: "Count".to_string(),
+                name: "A-B".to_string(),
+                count: 1,
             }
         );
     }
